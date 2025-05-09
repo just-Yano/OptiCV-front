1bf97b8abeb41deec668e791bc5a6fbcb90d56b2
Adrian <adriansaleh061@gmail.com>
2025-05-09 11:52:01 +0200

    page routing established

--- OptiCV-front/src/app/app.routes.ts
+++ OptiCV-front/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreationComponent } from './creation/creation.component';
+import { EvaluateComponent } from './evaluate/evaluate.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+    { path: 'home', component: HomeComponent },
+    { path: 'creation', component: CreationComponent}, 
+    { path: 'evaluation', component: EvaluateComponent},
 ];
---
+++ OptiCV-front/src/app/evaluate/evaluate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluate',
+  imports: [],
+  templateUrl: './evaluate.component.html',
+  styleUrl: './evaluate.component.css'
+})
+export class EvaluateComponent {
+
+}
