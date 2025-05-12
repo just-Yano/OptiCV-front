5bf7c3c99fb1ab8cb852755aed944e93955da1a5
gmarchimekari <gmarchimekari@gmail.com>
2025-05-12 17:26:57 +0200

    add section

---
+++ src/app/add-section/add-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-section',
+  imports: [],
+  templateUrl: './add-section.component.html',
+  styleUrl: './add-section.component.css'
+})
+export class AddSectionComponent {
+
+}
+
--- src/app/creation/creation.component.ts
+++ src/app/creation/creation.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AddSectionComponent } from '../add-section/add-section.component';
 
 @Component({
   selector: 'app-creation',
-  imports: [],
+  imports: [AddSectionComponent],
   templateUrl: './creation.component.html',
   styleUrl: './creation.component.css'
 })
