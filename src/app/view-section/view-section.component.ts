1824fe474c055590ff5a79b3e5b127770a63d875
gmarchimekari <gmarchimekari@gmail.com>
2025-05-14 11:20:25 +0200

    add button ok

--- src/app/view-section/view-section.component.ts
+++ src/app/view-section/view-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './view-section.component.css'
 })
 export class ViewSectionComponent {
+  @Input() showAddButton: boolean = false;
 
   // Sample data for the view section. TODO fetch from backend
   contact = [
