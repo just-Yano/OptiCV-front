490c01bb2a05c00b9121146239b94870002f5907
danielhabib3 <danielhabib301@gmail.com>
2025-05-14 09:47:31 +0200

    api call getEmptyTemplate

--- src/app/creation/creation.component.ts
+++ src/app/creation/creation.component.ts
@@ -24,199 +24,36 @@ export class CreationComponent {
   receiveTemplate(template: Template) {
     this.templateChosen = true;
     this.template = template;
+
+    fetch(`http://localhost:8080/api/cvtemplate/getEmptyTemplate?cvTemplateId=${this.template.id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch PDF');
+      }
+      return response.blob();
+    })
+    .then(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const iframe = document.querySelector('iframe'); // Select the iframe element
+      if (iframe) {
+        iframe.src = url; // Set the blob URL as the iframe's src
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+
   }
 
   generate() {
 
-    const latexCode = `%-------------------------
-    % Resume in Latex
-    % Author : Jake Gutierrez
-    % Based off of: https://github.com/sb2nov/resume
-    % License : MIT
-    %------------------------
-
-    \\documentclass[letterpaper,11pt]{article}
-
-    \\usepackage{latexsym}
-    \\usepackage[empty]{fullpage}
-    \\usepackage{titlesec}
-    \\usepackage{marvosym}
-    \\usepackage[usenames,dvipsnames]{color}
-    \\usepackage{verbatim}
-    \\usepackage{enumitem}
-    \\usepackage[hidelinks]{hyperref}
-    \\usepackage{fancyhdr}
-    \\usepackage[english]{babel}
-    \\usepackage{tabularx}
-    \\usepackage{fontawesome5}
-    \\usepackage{multicol}
-    \\setlength{\\multicolsep}{-3.0pt}
-    \\setlength{\\columnsep}{-1pt}
-    \\input{glyphtounicode}
-
-    %----------FONT OPTIONS----------
-    % sans-serif
-    % \\usepackage[sfdefault]{FiraSans}
-    % \\usepackage[sfdefault]{roboto}
-    % \\usepackage[sfdefault]{noto-sans}
-    % \\usepackage[default]{sourcesanspro}
-
-    % serif
-    % \\usepackage{CormorantGaramond}
-    % \\usepackage{charter}
-
-    \\pagestyle{fancy}
-    \\fancyhf{} % clear all header and footer fields
-    \\fancyfoot{}
-    \\renewcommand{\\headrulewidth}{0pt}
-    \\renewcommand{\\footrulewidth}{0pt}
-
-    % Adjust margins
-    \\addtolength{\\oddsidemargin}{-0.6in}
-    \\addtolength{\\evensidemargin}{-0.5in}
-    \\addtolength{\\textwidth}{1.19in}
-    \\addtolength{\\topmargin}{-.7in}
-    \\addtolength{\\textheight}{1.4in}
-
-    \\urlstyle{same}
-
-    \\raggedbottom
-    \\raggedright
-    \\setlength{\\tabcolsep}{0in}
-
-    % Sections formatting
-    \\titleformat{\\section}{
-      \\vspace{-4pt}\\scshape\\raggedright\\large\\bfseries
-    }{}{0em}{}[\\color{black}\\titlerule \\vspace{-5pt}]
-
-    % Ensure that generate pdf is machine readable/ATS parsable
-    \\pdfgentounicode=1
-
-    %-------------------------
-    % Custom commands
-    \\newcommand{\\resumeItem}[1]{
-      \\item\\small{
-        {#1 \\vspace{-2pt}}
-      }
-    }
-
-    \\newcommand{\\classesList}[4]{
-        \\item\\small{
-            {#1 #2 #3 #4 \\vspace{-2pt}}
-      }
-    }
-
-    \\newcommand{\\resumeSubheading}[4]{
-      \\vspace{-2pt}\\item
-        \\begin{tabular*}{1.0\\textwidth}[t]{l@{\\extracolsep{\\fill}}r}
-          \\textbf{#1} & \\textbf{\\small #2} \\\\
-          \\textit{\\small#3} & \\textit{\\small #4} \\\\
-        \\end{tabular*}\\vspace{-7pt}
-    }
-
-    \\newcommand{\\resumeSubSubheading}[2]{
-        \\item
-        \\begin{tabular*}{0.97\\textwidth}{l@{\\extracolsep{\\fill}}r}
-          \\textit{\\small#1} & \\textit{\\small #2} \\\\
-        \\end{tabular*}\\vspace{-7pt}
-    }
-
-    \\newcommand{\\resumeProjectHeading}[2]{
-        \\item
-        \\begin{tabular*}{1.001\\textwidth}{l@{\\extracolsep{\\fill}}r}
-          \\small#1 & \\textbf{\\small #2}\\\\
-        \\end{tabular*}\\vspace{-7pt}
-    }
-
-    \\newcommand{\\resumeSubItem}[1]{\\resumeItem{#1}\\vspace{-4pt}}
-
-    \\renewcommand\\labelitemi{$\\vcenter{\\hbox{\\tiny$\\bullet$}}$}
-    \\renewcommand\\labelitemii{$\\vcenter{\\hbox{\\tiny$\\bullet$}}$}
-
-    \\newcommand{\\resumeSubHeadingListStart}{\\begin{itemize}[leftmargin=0.0in, label={}]}
-    \\newcommand{\\resumeSubHeadingListEnd}{\\end{itemize}}
-    \\newcommand{\\resumeItemListStart}{\\begin{itemize}}
-    \\newcommand{\\resumeItemListEnd}{\\end{itemize}\\vspace{-5pt}}
-
-    %-------------------------------------------
-    %%%%%%  RESUME STARTS HERE  %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    \\begin{document}
-
-    %----------HEADING----------
-    % \\begin{tabular*}{\\textwidth}{l@{\\extracolsep{\\fill}}r}
-    %   \\textbf{\\href{http://sourabhbajaj.com/}{\\Large Sourabh Bajaj}} & Email : \\href{mailto:sourabh@sourabhbajaj.com}{sourabh@sourabhbajaj.com}\\\\
-    %   \\href{http://sourabhbajaj.com/}{http://www.sourabhbajaj.com} & Mobile : +1-123-456-7890 \\\\
-    % \\end{tabular*}
-    {\\Huge \\scshape First Last}\\\\ \\vspace{1pt}
-    \\section{ContactInfo}
-    \\begin{center}
-        Address \\\\ \\vspace{1pt}
-        \\small \\raisebox{-0.1\\height}\\faPhone\\ Telephone ~ \\href{mailto:x@x.x}{\\raisebox{-0.2\\height}\\faEnvelope\\  \\underline{Email}} ~ 
-        \\href{https://linkedin.com/in//}{\\raisebox{-0.2\\height}\\faLinkedin\\ \\underline{LinkedIn}}  ~
-        \\href{https://github.com/}{\\raisebox{-0.2\\height}\\faGithub\\ \\underline{Github}}
-        \\vspace{-8pt}
-    \\end{center}
-
-    %-----------EDUCATION-----------
-    \\section{Education}
-      \\resumeSubHeadingListStart
-        \\resumeSubheading
-          {Location}{Month Year -- Month Year}
-          {Degree}{City, State}
-      \\resumeSubHeadingListEnd
-
-    %------RELEVANT COURSEWORK-------
-    \\section{SoftSkills}
-        %\\resumeSubHeadingListStart
-            \\begin{multicols}{4}
-                \\begin{itemize}[itemsep=-5pt, parsep=3pt]
-                    \\item Soft Skill
-                \\end{itemize}
-            \\end{multicols}
-            \\vspace*{2.0\\multicolsep}
-        %\\resumeSubHeadingListEnd
-
-    %-----------EXPERIENCE-----------
-    \\section{Experience}
-      \\resumeSubHeadingListStart
-
-        \\resumeSubheading
-          {Company}{Month Year -- Month Year}
-          {JobTitle}{City, State}
-          \\resumeItemListStart
-            \\resumeItem{Description}
-          \\resumeItemListEnd
-        
-      \\resumeSubHeadingListEnd
-    \\vspace{-16pt}
-
-    %-----------PROJECTS-----------
-    \\section{Projects}
-        \\vspace{-5pt}
-        \\resumeSubHeadingListStart
-              \\resumeProjectHeading
-              {\\textbf{Title} \\emph{}}{Month Year}
-              \\resumeItemListStart
-                \\resumeItem{Description}
-              \\resumeItemListEnd 
-              
-        \\resumeSubHeadingListEnd
-    \\vspace{-15pt}
-
-    %-----------PROGRAMMING SKILLS-----------
-    \\section{HardSkills}
-        %\\resumeSubHeadingListStart
-            \\begin{multicols}{4}
-                \\begin{itemize}[itemsep=-5pt, parsep=3pt]
-                    \\item Hard Skill
-                \\end{itemize}
-            \\end{multicols}
-            \\vspace*{2.0\\multicolsep}
-        %\\resumeSubHeadingListEnd
-
-    \\end{document}`
-    ;
+    const latexCode = ``;  // TODO : get the final cv to download
   // You can dynamically build this from user inputs if needed
   
   
--- src/app/template-selector/template-selector.component.ts
+++ src/app/template-selector/template-selector.component.ts
@@ -34,37 +34,24 @@ export class TemplateSelectorComponent {
   constructor(private sanitizer: DomSanitizer) {}
 
 
-  ngOnInit() { // TODO put back the fetch
+  ngOnInit() {
     // getting the templates from the server
-    // fetch('http://localhost:8080/cvtemplates/getAllTemplates')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     // Assuming the data is an array of templates
-    //     this.templates = data;
-    //     this.templates.forEach(template => {
-    //       // Sanitize the thumbnail SVG string
-    //       template.safeThumbnail = this.sanitizer.bypassSecurityTrustHtml(template.thumbnail);
-    //     });
-    //   }
-    //   )
-    //   .catch(error => {
-    //     console.error('Error fetching templates:', error);
-    //   }
-    //   );
-
-    // Sample data TODO remove
-    this.templates = [
-      { id: 1, name: 'Modern Website', description: 'A sleek and modern website template.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>" , safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 2, name: 'E-Commerce Store', description: 'Perfect template for online stores with shopping cart functionality.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 3, name: 'Portfolio', description: 'Showcase your work and skills with this stylish portfolio template.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 4, name: 'Blog', description: 'Simple and clean template for a personal blog.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 5, name: 'Business Landing Page', description: 'A professional template for business landing pages.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 6, name: 'Photography Gallery', description: 'Perfect for photographers to showcase their work with a grid gallery.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>" ,   safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 7, name: 'News Website', description: 'Template for online news with an article layout and multimedia content.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 8, name: 'Restaurant Menu', description: 'Beautiful template designed for restaurant websites with menus and ordering functionality.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 9, name: 'Creative Agency', description: 'A modern and vibrant template for creative agencies and digital studios.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-      { id: 10, name: 'Educational Platform', description: 'Perfect template for educational institutions and online learning platforms.', thumbnail: "<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>", safeThumbnail: this.sanitizer.bypassSecurityTrustHtml("<svg><rect x='0' y='0' width='100' height='100' stroke='black' fill='white' stroke-width='2'/></svg>") },
-    ];
+    fetch('http://localhost:8080/api/cvtemplate/getAllTemplates')
+      .then(response => response.json())
+      .then(data => {
+        // Assuming the data is an array of templates
+        this.templates = data;
+        console.log(data);
+        this.templates.forEach(template => {
+          // Sanitize the thumbnail SVG string
+          template.safeThumbnail = this.sanitizer.bypassSecurityTrustHtml(template.thumbnail);
+        });
+      }
+      )
+      .catch(error => {
+        console.error('Error fetching templates:', error);
+      }
+      );
   }
 
   onPageChange(page: number) {
