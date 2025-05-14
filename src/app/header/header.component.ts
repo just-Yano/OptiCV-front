9bbf5d37a8324c82f9a393f3c1c853715645cc0f
gmarchimekari <gmarchimekari@gmail.com>
2025-05-14 21:07:27 +0200

    merge tailor

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -43,7 +43,7 @@ export class HeaderComponent {
       this.navigationService.navigateToHome();
     }   
   }
-  
+
   navigateToTailoring() {
     this.navigationService.navigateToTailoring();
   }
