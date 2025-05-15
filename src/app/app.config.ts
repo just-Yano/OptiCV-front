22f9d1a13bb83847c790a72cba8a48b9a2a57102
gmarchimekari <gmarchimekari@gmail.com>
2025-05-15 11:48:42 +0200

    toaster removal

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
